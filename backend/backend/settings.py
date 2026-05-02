7ddcfa02a04e79dbef3cd00103c460a4a6c2c365
Raufu Abdulraman <coder0214h@gmail.com>
2026-05-02 17:57:45 +0100

    (whip): Backend CORS_ALL for hackathon

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,10 +124,7 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # Database
